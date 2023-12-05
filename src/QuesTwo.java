2f353d75d9c0484eaacab5d0e1d083d92cec3795
RealGuzu <147645744+RealGuzu@users.noreply.github.com>
2023-12-05 13:35:32 -0500

    Update QuesTwo.java

--- src/QuesTwo.java
+++ src/QuesTwo.java
@@ -1,24 +1,34 @@
 import java.util.Scanner;
 
 public class QuesTwo {
-    static int factorial(int n) {
-        if (n == 0)
+    
+    private int number;
+    private int fact;
+
+    public QuesTwo(int number) {
+        this.number = number;
+        this.fact = factorial(number);
+    }
+
+    public static int factorial(int n) {
+        if (n == 0) {
             return 1;
-        else
+        } else {
             return n * factorial(n - 1);
+        }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int fact;
+        int enteredNumber;
 
         System.out.print("Enter the number: ");
-        int number = scanner.nextInt();
+        enteredNumber = scanner.nextInt();
 
-        fact = factorial(number);
+        QuesTwo obj = new QuesTwo(enteredNumber);
 
-        System.out.println("Factorial of " + number + " is: " + fact);
+        System.out.println("Factorial of " + obj.number + " is: " + obj.fact);
 
         scanner.close();
     }
